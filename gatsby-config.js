b47f8912b6c27fb791c811426f37b45ebd7793d9
maximealc <58790299+maximealc@users.noreply.github.com>
2022-12-19 15:45:01 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `maximall - Blog`,
+    siteTitleAlt: `maximall - Blog`,
+    siteHeadline: `maximall - Blog`,
+    siteUrl: `https://www.maximall.live`,
+    siteDescription: `Simply a place to gather my thoughts.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@maximall`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -37,11 +37,7 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://twitter.com/_maximall`,
           },
         ],
       },
